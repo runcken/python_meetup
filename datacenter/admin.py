9487d08def80fd50ae081e5f4abd3bc896c69106
runcken <runcken@gmail.com>
2025-11-26 00:42:41 +0300

    add models

--- datacenter/admin.py
+++ datacenter/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import (
+    Event,
+    Speaker,
+    Speech,
+    Participant,
+    Question,
+    Subscription
+)
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'is_active']
+    list_filter = ['is_active', 'date']
+
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'telegram_id']
+
+
+@admin.register(Speech)
+class SpeechAdmin(admin.ModelAdmin):
+    list_display = [
+       'title',
+       'speaker',
+       'event',
+       'start_time',
+       'end_time',
+       'is_active'
+    ]
+    list_filter = ['event', 'is_active']
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'company', 'position', 'registered_at']
+    serch_fields = ['full_name', 'company']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['participant', 'speech', 'created_at', 'is_answered']
+    list_filter = ['speech', 'is_answered']
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['participant', 'event', 'subscribed_at']
--- datacenter/models.py
+++ datacenter/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField('Название', max_length=255)
+    description = models.TextField('Описание')
+    date = models.DateTimeField('Дата')
+    is_active = models.BooleanField('Активно', default=False)
+    created_at = models.DateTimeField('Создано', auto_now_add=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'Конференция'
+        verbose_name_plural = 'Конференции'
+
+    def __str__(self):
+        return self.title
+
+
+class Speaker(models.Model):
+    name = models.CharField('Имя', max_length=255)
+    telegram_id = models.BigIntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Спикер'
+
+    def __str__(self):
+        return self.title
+
+
+class Speech(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('event',)
+        verbose_name = 'Презентация'
+        verbose_name_plural = 'Презентации'
+
+    def __str__(self):
+        return self.title
+
+
+class Participant(models.Model):
+    telegram_id = models.BigIntegerField(unique=True, blank=True)
+    username = models.CharField(max_length=255, blank=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    company = models.CharField(max_length=255, blank=True)
+    position = models.CharField(max_length=255, blank=True)
+    experience = models.CharField(max_length=100, blank=True)
+    registered_at = models.DateTimeField(blank=True)
+
+    class Meta:
+        ordering = ('registered_at',)
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    speech = models.ForeignKey(Speech, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_answered = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('speech',)
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def __str__(self):
+        return self.title
+
+
+class Subscription(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('participant',)
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return self.title
